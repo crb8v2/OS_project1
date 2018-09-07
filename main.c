17a5e91ed20219a25b5007494fe230ed4840a0b1
Connor <connor@admin.com>
2018-09-07 16:29:01 -0500

    getopt is working, the following url was referenced 	https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
    to do:
    check for proper number of command line args
    perror

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// test comment
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h> // for perror
@@ -8,8 +8,37 @@
 
 #include <sys/wait.h>
 
+void forkerMaster (int n, int k, int m) {
+
+    pid_t childpid = 0;
+    int counter1, counter2;
+
+    for (counter1 = 1; counter1 < n; counter1++) {
+        sleep(2);
+        if((childpid = fork()))
+            break;
+    }
+
+    for (counter2 = 0; counter2 < k; counter2++) {
+
+        fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", counter1,
+                (long) getpid(), (long) getppid(), (long) childpid);
+
+//        fprintf(stderr, "i:%d", counter1);
+//        fprintf(stderr, "process ID:%ld ", (long)getpid());
+//        fprintf(stderr, "parent ID:%ld ", (long)getppid());
+//        fprintf(stderr,"child ID:%ld\n", (long)childpid);
+
+        sleep(m);
+    }
+
+}
+
 void helpPrint() {
-    printf("\nwere in help\n");
+    printf("\n -- HELP MENU --\n");
+    printf("-n int int int    runs a forking function\n");
+    printf("-h    open help menu\n");
+    printf("-p    generates a test error with perror\n\n");
 }
 
 void errorPrint() {
@@ -20,14 +49,20 @@ void newPrint() {
     printf("\nwere in new\n");
 }
 
+
+
 int main (int argc, char **argv) {
-    int aflag = 0;
-    int bflag = 0;
+
     char *cvalue = NULL;
     int index;
-    int c;
+    int n, k, m, c;
 
     opterr = 0;
+
+    // get command line values
+    n = atoi(argv[2]);
+    k = atoi(argv[3]);
+    m = atoi(argv[4]);
 
 
     while ((c = getopt (argc, argv, "hpn:")) != -1)
@@ -35,14 +70,13 @@ int main (int argc, char **argv) {
         {
             case 'h':
                 helpPrint();
-                aflag = 1;
                 break;
             case 'p':
                 errorPrint();
-                bflag = 1;
                 break;
             case 'n':
-                newPrint();
+                //get and arg checker in here!
+                forkerMaster(n,k,m);
                 cvalue = optarg;
                 break;
             case '?':
@@ -58,105 +92,78 @@ int main (int argc, char **argv) {
             default:
                 abort ();
         }
-
-
-    printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-            aflag, bflag, cvalue);
-
-    for (index = optind; index < argc; index++)
-        printf ("Non-option argument %s\n", argv[index]);
-
     return 0;
 }
 
 
-//void helpPrint() {
-//    printf("\nwere in help\n");
-//}
-//
-//
-//int main (int argc, char **argv) {
-//    int aflag = 0;
-//    int bflag = 0;
-//    char *cvalue = NULL;
-//    int index;
-//    int c;
-//
-//    opterr = 0;
-//
-//
-//    while ((c = getopt (argc, argv, "hb:")) != -1)
-//        switch (c)
-//        {
-//            case 'h':
-//                helpPrint();
-//                aflag = 1;
-//                break;
-//            case 'b':
-//                bflag = 1;
-//                break;
-//            case '?':
-//                if (optopt == 'c')
-//                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-//                else if (isprint (optopt))
-//                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-//                else
-//                    fprintf (stderr,
-//                             "Unknown option character `\\x%x'.\n",
-//                             optopt);
-//                return 1;
-//            default:
-//                abort ();
-//        }
-//
-//
-//    printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-//            aflag, bflag, cvalue);
-//
-//    for (index = optind; index < argc; index++)
-//        printf ("Non-option argument %s\n", argv[index]);
-//    return 0;
-//}
 
 
 
 
 
+
+
+
+//
 //void helpPrint() {
 //    printf("\nwere in help\n");
 //}
 //
 //int main (int argc, char **argv) {
-//
-//    pid_t childpid = 0;
-//    int counter1, counter2;
-//    int c, n, k, m;
-//
-//    while ((c = getopt(argc, argv, "h:")) != -1)
-//        switch (c) {
-//            case 'h':
-//                helpPrint();
-//                break;
-//            default:
-//                abort();
-//
-//        }
-//
-//    return 0;
-//}
-//
-
-
+////
+////    pid_t childpid = 0;
+////    int counter1, counter2;
+//    int n, k, m;
+////
+////
+////
+////
 //    // checks for proper number of args
 //    if (argc != 4) {
 //        fprintf(stderr, "Invalid number of command line args.");
 //        return 1;
 //    }
-//
+////
 //    // get command line values
 //    n = atoi(argv[1]);
 //    k = atoi(argv[2]);
 //    m = atoi(argv[3]);
+////
+////
+////    for (counter1 = 1; counter1 < n; counter1++) {
+////        sleep(2);
+////        if((childpid = fork()))
+////            break;
+////    }
+////
+////
+////    for (counter2 = 0; counter2 < k; counter2++) {
+////
+////        fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", counter1,
+////                (long) getpid(), (long) getppid(), (long) childpid);
+////
+//////        fprintf(stderr, "i:%d", counter1);
+//////        fprintf(stderr, "process ID:%ld ", (long)getpid());
+//////        fprintf(stderr, "parent ID:%ld ", (long)getppid());
+//////        fprintf(stderr,"child ID:%ld\n", (long)childpid);
+////
+////        sleep(m);
+////    }
+//    return 0;
+//}
+//
+//
+//
+//void forkerMaster (int n, int k, int m) {
+//
+//    pid_t childpid = 0;
+//    int counter1, counter2;
+//
+//
+////    // get command line values
+////    n = atoi(argv[1]);
+////    k = atoi(argv[2]);
+////    m = atoi(argv[3]);
 //
 //
 //    for (counter1 = 1; counter1 < n; counter1++) {
@@ -168,17 +175,17 @@ int main (int argc, char **argv) {
 //
 //    for (counter2 = 0; counter2 < k; counter2++) {
 //
-////        fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", counter1,
-////                (long) getpid(), (long) getppid(), (long) childpid);
+//        fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", counter1,
+//                (long) getpid(), (long) getppid(), (long) childpid);
 //
-//        fprintf(stderr, "i:%d", counter1);
-//        fprintf(stderr, "process ID:%ld ", (long)getpid());
-//        fprintf(stderr, "parent ID:%ld ", (long)getppid());
-//        fprintf(stderr,"child ID:%ld\n", (long)childpid);
+////        fprintf(stderr, "i:%d", counter1);
+////        fprintf(stderr, "process ID:%ld ", (long)getpid());
+////        fprintf(stderr, "parent ID:%ld ", (long)getppid());
+////        fprintf(stderr,"child ID:%ld\n", (long)childpid);
 //
 //        sleep(m);
 //    }
-//    return 0;
+//
 //}
-
-
+//
+//
